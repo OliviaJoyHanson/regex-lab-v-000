9a86b9ac9fec590a54fec167d6949d33d3c553f4
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-18 16:57:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(string)
 end
 
 def valid_phone_number?(number)
-  if number.scan(/\A\d{10}\z/)
+  if number.match(/\A\d{10}\z/)
     true
   else
     false
