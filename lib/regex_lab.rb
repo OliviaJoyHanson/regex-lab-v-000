9201257b66556d4396943fc360d0baad1ef7a00f
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-18 15:59:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,3 +5,15 @@ end
 def words_starting_with_un_and_ending_with_ing(word)
 
 end
+
+def words_five_letters_long(word)
+
+end
+
+def first_word_capitalized_and_ends_with_puncutation?(word)
+
+end
+
+def valid_phone_number?(number)
+
+end
